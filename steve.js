00d2fe3c3e1ecf38acd2208a5ce8cf245269131e
Steve <antonsteve05@gmail.com>
2026-01-27 22:40:11 +0100

    Permis B

--- steve.js
+++ steve.js
@@ -1416,6 +1416,12 @@ const translations = {
         'skill-ide': 'IDE',
         
         // Certifications
+
+        'cert-permis-b': 'Permis B',
+        "cert-permis-b-issuer": "Permis de conduire catégorie B",
+        "cert-permis-b-date": "Obtenu en 2026",
+        "cert-permis-b-desc": "Titulaire du permis B depuis 2026",
+        
         'cert-child-protection': 'Protéger l\'enfance',
         'cert-child-protection-issuer': 'Diocèse de Saint Denis - MOOCit',
         'cert-child-protection-date': 'Obtenu le 5 juin 2025',
@@ -1643,6 +1649,12 @@ const translations = {
         'skill-ide': 'IDE',
         
         // Certifications
+
+        "cert-permis-b": "Driving License B",
+        "cert-permis-b-issuer": "Category B Driving License",
+        "cert-permis-b-date": "Obtained in 2026",
+        "cert-permis-b-desc": "Holder of the B license since 2026",
+
         'cert-child-protection': 'Child Protection',
         'cert-child-protection-issuer': 'Diocese of Saint Denis - MOOCit',
         'cert-child-protection-date': 'Obtained on June 5, 2025',
